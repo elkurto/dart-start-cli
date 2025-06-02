2647ba4d64f86152f9b1aa84dbc007af232d0715
elkurto <kurt.buehner@gmail.com>
2025-06-02 08:43:18 -0400

    added proper async method

--- aaa_dart_cli/bin/011_async_prog.dart
+++ aaa_dart_cli/bin/011_async_prog.dart
@@ -5,27 +5,38 @@ Future<void> main(List<String> args) async {
 
   longPressSleep(); // a synchronous wait
   longPressFuture(); // an async wait
+  print("done with longPressSleep and longPressFuture");
 
-  Future<User> futureUser00 = createFutureWithReturnValue(0);
+  // a. runing an async-wrapper "createUserAsync" around an async fn
+  //    works 100% asynchronously.
+  //    Preferred implementation
+  print("calling createUserAsync");
+  createUserAsync(2, "baz", true);
+  print("called createUserAsync");
+
+  /// b. Running an async method that returns future in main-thread.
+  Future<User> futureUser00 = createFutureWithReturnValue(0, 'foo', true);
   futureUser00.then((user00) => print("user00 =${user00.toString()}"));
 
-  User user01 = await createFutureWithReturnValue(1);
+  // Awaiting async-methods in main
+  //  just runs the method synchronously.
+  /// c. don't do this
+  User user01 = await createFutureWithReturnValue(1, 'bar', true);
   print("user01 =${user01.toString()}");
-  print("bbbb");
+  print("done main");
 }
 
 longPressSleep() {
   int millis = 1000;
   print("waiting $millis ms");
   sleep(Duration(milliseconds: 1000));
-  print("done waiting");
+  print("in longPressSleep -- done waiting in longPressSleep");
 }
 
 longPressFuture() {
   int millis = 1000;
   Future.delayed(Duration(milliseconds: millis), () {
-    print("done with future delay of $millis ms");
-    print("done");
+    print("in longPressFuture -- done with future delay of $millis ms");
   });
 }
 
@@ -42,10 +53,15 @@ class User {
   }
 }
 
-Future<User> createFutureWithReturnValue(int id) {
+void createUserAsync(int id, String name, bool active) async {
+  User user = await createFutureWithReturnValue(id, name, active);
+  print("in createUserAsync user =${user.toString()}");
+}
+
+Future<User> createFutureWithReturnValue(int id, String name, bool active) {
   int millis = 1000;
 
   return Future<User>.delayed(Duration(milliseconds: millis), () {
-    return User(id: id, name: 'foo', active: true);
+    return User(id: id, name: name, active: active);
   });
 }
