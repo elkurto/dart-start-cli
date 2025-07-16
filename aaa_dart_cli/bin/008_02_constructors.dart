efe25b0ec08703261057eedb19ce16096a2e0f48
elkurto <kurt.buehner@gmail.com>
2025-07-16 18:09:52 -0400

    const ctor example -- sub object (HashMap) must be static final for const ctor to work

--- aaa_dart_cli/bin/008_02_constructors.dart
+++ aaa_dart_cli/bin/008_02_constructors.dart
@@ -2,27 +2,56 @@
 // https://dart.dev/language/constructors#constant-constructors
 import 'dart:collection';
 
-const Map<String, String> constMap = {}; // declare
+// const Map<String, String> constMap = {}; // declare and use global map -- might just as well be static
 
 class ArgResult {
   //final Map<String,String> mapNameArg =HashMap(); // fails with error about non-const initial value.
   //final Map<String,String> mapNameArg ={}; // fails with error about non-const initial value.
 
-  final Map<String, String> mapNameArg = constMap; // strangely okay
+  // final Map<String, String> mapNameArg = constMap; // strangely okay
+  static final Map<String, String> mapNameArg =HashMap();  // also okay
 
   const ArgResult(); // const ctor have no-body
 
   ArgResult initFromListString(List<String> args) {
+    String arg;
+    for (int i =0 ; i < args.length; i++ ) {
+      print( "i =${i} and args[${i}] =${args[i]}");
+      arg =args[i];
+      if ( arg.startsWith("-")) {
+        
+        if (args.length > i+1 ) {
+          if (args[i+1].startsWith("-")) {
+            mapNameArg[arg] ="";
+          }else {
+            mapNameArg[arg] =args[i+1];
+            i += 1;
+          }
+        }else {
+          mapNameArg[arg] ="";
+        }
+      }
+    }//end for 
     return this;
   }
 
   factory ArgResult.fromListString(List<String> args) {
     const ArgResult argResult = ArgResult();
-
+    argResult.initFromListString(args);
     return argResult;
+  }
+
+  String toString() {
+    StringBuffer sb =StringBuffer();
+    for ( var key in mapNameArg.keys) {
+      sb.writeln( "  ${key} =${mapNameArg[key]}");
+    }
+    return sb.toString();
   }
 }
 
 void main(List<String> args) {
-  ArgResult argResult = ArgResult().initFromListString(args);
+  ArgResult argResult = ArgResult.fromListString(args);
+  print("argResult =\n${argResult.toString()}");
+  
 }
