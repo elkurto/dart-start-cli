6aa9d5db969ed7127adec610f065291551f49a62
elkurto <kurt.buehner@gmail.com>
2025-08-21 19:42:16 -0400

    t00_unittest000.dart :: added doc

--- aaa_dart_cli/bin/t00_unittest000.dart
+++ aaa_dart_cli/bin/t00_unittest000.dart
@@ -13,3 +13,15 @@ void main() {
     });
   });
 }
+
+/*
+Run with the command : 
+   dart t00_unittest000.dart 
+
+   Output:
+    00:00 +0: test-String test-startsWith
+
+    00:00 +1: test-String test-endsWith
+
+    00:00 +2: All tests passed!
+ */
