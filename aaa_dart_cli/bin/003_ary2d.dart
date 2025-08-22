e3c21c89ef48b31c94dba6f3b234afb77722abab
elkurto <kurt.buehner@gmail.com>
2025-08-22 19:16:19 -0400

    003_ary2d.dart - added doc

--- aaa_dart_cli/bin/003_ary2d.dart
+++ aaa_dart_cli/bin/003_ary2d.dart
@@ -25,3 +25,11 @@ Iterable<Cell> createIterableOfCell(final int ncol, final int nrow) {
 void main(List<String> args) {
   List<Cell> listCell = List.of(createIterableOfCell(5, 5), growable: false);
 }
+
+/*
+Loosely based on examples from 
+https://api.flutter.dev/flutter/dart-core/Iterable/Iterable.generate.html.
+But the example lacked context so I had to google and experiment.
+I am not sure why it's so difficult to create a fixed length List<Object> in dart.
+WTF 
+*/
