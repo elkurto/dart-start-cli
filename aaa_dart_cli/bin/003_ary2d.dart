aeaa9643e020706328d60b7aefb04e51fdcf4cde
elkurto <kurt.buehner@gmail.com>
2025-08-23 08:48:38 -0400

    added main Grid class

--- aaa_dart_cli/bin/003_ary2d.dart
+++ aaa_dart_cli/bin/003_ary2d.dart
@@ -3,8 +3,8 @@ class Cell {
   int irow;
 
   Cell(this.icol, this.irow);
-  factory Cell.makeOne(index, ncol, nrow) {
-    return Cell(index % ncol, index / ncol);
+  factory Cell.makeOne(int index, int ncol, int nrow) {
+    return Cell(index % ncol, (index / ncol).floor());
   }
 
   @override
@@ -13,17 +13,29 @@ class Cell {
   }
 }
 
-Iterable<Cell> createIterableOfCell(final int ncol, final int nrow) {
-  int nelement = ncol * nrow;
-  Iterable<Cell> iterable = Iterable.generate(
-    nelement,
-    (index) => Cell.makeOne(index, ncol, nrow),
-  );
-  return iterable;
+class Grid {
+  late List<Cell> listCell;
+  int nrow;
+  int ncol;
+
+  Grid(this.ncol, this.nrow) {
+    listCell = List.of(createIterableOfCell(5, 5), growable: false);
+  }
+
+  Iterable<Cell> createIterableOfCell(final int ncol, final int nrow) {
+    int nelement = ncol * nrow;
+    Iterable<Cell> iterable = Iterable.generate(
+      nelement,
+      (index) => Cell.makeOne(index, ncol, nrow),
+    );
+    return iterable;
+  }
 }
 
 void main(List<String> args) {
-  List<Cell> listCell = List.of(createIterableOfCell(5, 5), growable: false);
+  Grid grid = Grid(5, 5);
+  assert(grid.listCell.length == 25);
+  print("grid.listCell.length =${grid.listCell.length}");
 }
 
 /*
