ac75cf03e26c789642abfc91e0e872f7f2da3883
elkurto <kurt.buehner@gmail.com>
2025-08-24 09:06:55 -0400

    created test aaa_dart_cli/test/test_aba_singlton.dart

--- aaa_dart_cli/test/test_aba_singlton.dart
+++ aaa_dart_cli/test/test_aba_singlton.dart
@@ -1,1 +1,26 @@
-@todo 
+import "package:test/test.dart";
+import "package:aaa_dart_cli/aba_singleton.dart";
+
+main() {
+  group("test aba_singleton:::", () {
+    test("Singleton create", () {
+      CartModel cartModelInstance = CartModel.instance;
+      expect(cartModelInstance, isNotNull);
+      expect(cartModelInstance.mapIdItem.isEmpty, isTrue);
+
+      CartModel cartModelInstanceB = CartModel.instance;
+      expect(cartModelInstance, cartModelInstanceB);
+    });
+
+    test("Cart:: add remvoe", () {
+      CartModel cartModelInstance = CartModel.instance;
+      Item item1 = Item(1, "chile sauce");
+      cartModelInstance.addItem(item1);
+
+      expect(cartModelInstance.mapIdItem[1], item1);
+
+      cartModelInstance.removeItem(item1);
+      expect(cartModelInstance.mapIdItem.isEmpty, isTrue);
+    });
+  });
+}
