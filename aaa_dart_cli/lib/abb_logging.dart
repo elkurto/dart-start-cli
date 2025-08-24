16cfcc6626d5d9e1245f9443c2e613fab1470250
elkurto <kurt.buehner@gmail.com>
2025-08-24 16:13:24 -0400

    aaa_dart_cli/lib/abb_logging.dart :: added doc

--- aaa_dart_cli/lib/abb_logging.dart
+++ aaa_dart_cli/lib/abb_logging.dart
@@ -1,5 +1,9 @@
 import "package:logging/logging.dart";
 
+/*
+ *   Simple usage of :package: logging 
+ *   @see https://pub.dev/packages/logging
+ */
 void configLogger() {
   // set the root logging level
   Logger.root.level = Level.ALL;
@@ -42,3 +46,19 @@ void main(List<String> args) {
   exerciseLoggerInfo();
   exerciseLoggerSevere();
 }
+
+/*
+(@see https://pub.dev/packages/logging)
+Run example: 
+  dart lib/abb_logging.dart
+
+  OUTPUT:
+    INFO: 2025-08-24 15:46:40.927822: i am just and info message
+    INFO: 2025-08-24 15:46:40.933330: message in a closure -- delayed evaluation
+    SEVERE: 2025-08-24 15:46:40.933821: e.message =Exception: Whoopsie! #0      exerciseLoggerSevere (package:aaa_dart_cli/abb_logging.dart:34:5)
+    #1      main (package:aaa_dart_cli/abb_logging.dart:43:3)
+    #2      _delayEntrypointInvocation.<anonymous closure> (dart:isolate-patch/isolate_patch.dart:312:33)
+    #3      _RawReceivePort._handleMessage (dart:isolate-patch/isolate_patch.dart:193:12)
+
+
+ */
