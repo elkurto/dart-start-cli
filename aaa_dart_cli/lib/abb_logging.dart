1a1ac2e5885297bd71d406ad807c79b1596b4ddd
elkurto <kurt.buehner@gmail.com>
2025-08-24 11:18:25 -0400

    aaa_dart_cli/lib/abb_logging.dart

---
+++ aaa_dart_cli/lib/abb_logging.dart
@@ -0,0 +1,38 @@
+import "package:logging/logging.dart";
+
+void configLogger() {
+  // set the root logging level
+  Logger.root.level = Level.ALL;
+
+  // configure a listener for :event:record
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message}');
+  });
+
+  // optional -- listen for :event:levelChnaged
+  Logger.root.onLevelChanged.listen((level) {
+    print('The new log level is $level');
+  });
+}
+
+void exerciseLoggerInfo() {
+  final log = Logger('in :fn:exerciseLoggerInfo()');
+  log.info("i am just and info message");
+
+  log.info(() => "message in a closure -- delayed evaluation");
+}
+
+void exerciseLoggerSevere() {
+  final log = Logger('in :fn:exerciseLoggerSevere()');
+  try {
+    throw Exception("Whoopsie!");
+  } catch (e, stackTrace) {
+    log.severe("e.message =${e.toString()}", e, stackTrace);
+  }
+}
+
+void main(List<String> args) {
+  configLogger();
+  exerciseLoggerInfo();
+  exerciseLoggerSevere();
+}
