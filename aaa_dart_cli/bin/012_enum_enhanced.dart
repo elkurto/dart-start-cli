af098a8af2ecd8426039a0a3d924496eef6681a9
elkurto <kurt.buehner@gmail.com>
2025-10-22 07:46:55 -0400

    012_enum_enhanced : added main and print

--- aaa_dart_cli/bin/012_enum_enhanced.dart
+++ aaa_dart_cli/bin/012_enum_enhanced.dart
@@ -1,13 +1,20 @@
+// src = https://dart.dev/language/enums#declaring-enhanced-enums
 enum Vehicle implements Comparable<Vehicle> {
-  car(tires: 4, passengers: 5, carbonPerKilometer: 400),
-  bus(tires: 6, passengers: 50, carbonPerKilometer: 800),
-  bicycle(tires: 2, passengers: 1, carbonPerKilometer: 0);
+  car(tires: 4, passengers: 5, carbonPerKilometer: 400, name: "car"),
+  bus(tires: 6, passengers: 50, carbonPerKilometer: 800, name: "bus"),
+  bicycle(tires: 2, passengers: 1, carbonPerKilometer: 0, name: "bicycle");
 
-  const Vehicle({required this.tires, required this.passengers, required this.carbonPerKilometer});
+  const Vehicle({
+    required this.tires,
+    required this.passengers,
+    required this.carbonPerKilometer,
+    required this.name,
+  });
 
   final int tires;
   final int passengers;
   final int carbonPerKilometer;
+  final String name;
 
   int get carbonFootprint => (carbonPerKilometer / passengers).round();
 
@@ -15,4 +22,20 @@ enum Vehicle implements Comparable<Vehicle> {
 
   @override
   int compareTo(Vehicle other) => carbonFootprint - other.carbonFootprint;
+
+  @override
+  String toString() {
+    return "$name : carbonPerKm =$carbonFootprint ";
+  }
 }
+
+void main(List<String> arguments) {
+  for (Vehicle v in Vehicle.values) {
+    print(v);
+  }
+}
+
+// output: 
+//   car : carbonPerKm =80 
+//   bus : carbonPerKm =16 
+//   bicycle : carbonPerKm =0 
