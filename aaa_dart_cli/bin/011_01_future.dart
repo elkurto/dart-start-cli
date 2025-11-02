7de1b2b9d31a4da900175804f5377da61626e695
elkurto <kurt.buehner@gmail.com>
2025-11-02 18:51:38 -0500

    011_01_future.dart -based on netninja - flutter masterclass

--- aaa_dart_cli/bin/011_01_future.dart
+++ aaa_dart_cli/bin/011_01_future.dart
@@ -1,7 +1,20 @@
-void main() {
+void main() async {
+  //method 1
   fetchPost().then((post) {
-    print("Resolved to post =$post");
+    print("Resolved to post1 =$post");
   });
+  // method 2
+  Post post2 = await fetchPostAsync();
+  print("Resolved to post2 =$post2");
+
+  // method 3
+  Post post3 = await fetchPost();
+  print("Resolved to post3 =$post3");
+}
+
+Future<Post> fetchPostAsync() async {
+  final Post post = await fetchPost();
+  return post;
 }
 
 Future<Post> fetchPost() {
