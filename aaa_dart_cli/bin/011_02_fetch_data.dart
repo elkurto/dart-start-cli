0ecbe02d6201c81c7cd02413418cac3c646df160
elkurto <kurt.buehner@gmail.com>
2025-11-03 06:34:05 -0500

    011_02_fetch_dart added example

--- aaa_dart_cli/bin/011_02_fetch_data.dart
+++ aaa_dart_cli/bin/011_02_fetch_data.dart
@@ -1,43 +1,33 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 // install with `dart pub add http`
+// see https://pub.dev/packages/http
 // see https://pub.dev/packages/http/example
 
 void main() async {
-  //method 1
-  fetchPost().then((post) {
-    print("Resolved to post1 =$post");
-  });
-  // method 2
-  Post post2 = await fetchPostAsync();
-  print("Resolved to post2 =$post2");
-
-  // method 3
-  Post post3 = await fetchPost();
-  print("Resolved to post3 =$post3");
+  Future<JsonResponse> res = BookApi.pullBooksV01Json(topic: 'http');
+  print("res =$res");
 }
 
-Future<Post> fetchPostAsync() async {
-  final Post post = await fetchPost();
-  return post;
-}
+class JsonResponse {
+  String responseText;
+  int statusCode; // e.g. 200
 
-Future<Post> fetchPost() {
-  const delay = Duration(seconds: 3);
-
-  return Future.delayed(delay, () {
-    return Post('my post', 123);
-  });
-}
-
-class Post {
-  String title;
-  int userId;
-
-  Post(this.title, this.userId);
+  JsonResponse(this.responseText, this.statusCode);
 
   @override
   String toString() {
-    return "Post($userId, $title)";
+    return "JsonResponse($statusCode, $responseText)";
+  }
+}
+
+class BookApi {
+  static Future<JsonResponse> pullBooksV01Json({required String topic}) async {
+    var url = Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{$topic}'});
+    var response = await http.get(url);
+    JsonResponse jsonResponse = JsonResponse(response.body, response.statusCode);
+
+    return jsonResponse;
   }
 }
