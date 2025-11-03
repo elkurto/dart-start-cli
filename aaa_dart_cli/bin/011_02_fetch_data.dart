6309f24c1628a86c45dcc6f606a653792cce2550
elkurto <kurt.buehner@gmail.com>
2025-11-03 06:34:43 -0500

    011_02_fetch_dart exapmle typicode

--- aaa_dart_cli/bin/011_02_fetch_data.dart
+++ aaa_dart_cli/bin/011_02_fetch_data.dart
@@ -5,9 +5,11 @@ import 'dart:convert' as convert;
 // see https://pub.dev/packages/http
 // see https://pub.dev/packages/http/example
 
-void main() async {
+void main() {
   Future<JsonResponse> res = BookApi.pullBooksV01Json(topic: 'http');
-  print("res =$res");
+  res.then((res) {
+    print("res =$res");
+  });
 }
 
 class JsonResponse {
@@ -23,11 +25,20 @@ class JsonResponse {
 }
 
 class BookApi {
+  // https://www.googleapis.com/books/v1/volumes?q={http}
   static Future<JsonResponse> pullBooksV01Json({required String topic}) async {
     var url = Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{$topic}'});
-    var response = await http.get(url);
+    http.Response response = await http.get(url);
     JsonResponse jsonResponse = JsonResponse(response.body, response.statusCode);
 
     return jsonResponse;
+  }
+
+  // https://jsonplaceholder.typicode.com/guide/
+  // https://jsonplaceholder.typicode.com/posts/1
+  static Future<http.Response> pullFromTypicode() {
+    var url = Uri.https("jsonplaceholder.typicode.com/", "posts/1");
+    Future<http.Response> futureOfResponse = http.get(url);
+    return futureOfResponse;
   }
 }
