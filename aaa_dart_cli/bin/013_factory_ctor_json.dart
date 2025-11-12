43dc3580a1351711762f98df5fde9d6b088cb23d
elkurto <kurt.buehner@gmail.com>
2025-11-12 08:03:10 -0500

    013_factory_ctor_json.dart : doc

--- aaa_dart_cli/bin/013_factory_ctor_json.dart
+++ aaa_dart_cli/bin/013_factory_ctor_json.dart
@@ -1,6 +1,13 @@
 //import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
+/**
+ * USAGE:
+ * > dart bin/013_factory_ctor_json.dart 
+ *   Book(id=1,name=satan burger,isbn=1111-1111-1111-11)
+ *   Book(id=2,name=ultra f*ers,isbn=1111-1111-1111-12)
+ */
+
 void main() async {
   final List<Book> listBook = await fetchListBook();
 
